5cf4a231c31e044dd183fb6ea77d8955ba8f2959
pabloa1311 <pabloa1311@gmail.com>
2021-05-10 13:15:45 -0500

    sidebar Header created

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
+
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import { Avatar, IconButton } from "@material-ui/core";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <h1>sidebar</h1>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <QuestionAnswerIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
