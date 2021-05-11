03afff0c63bfc85ae838080553d6e6f6f4bb187f
pabloa1311 <pabloa1311@gmail.com>
2021-05-11 15:25:36 -0500

    App connected to firebase

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
+import db from "../firebase"
 
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -9,6 +10,19 @@ import { Avatar, IconButton } from "@material-ui/core";
 import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => 
+      setRooms(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          data: doc.data(),
+        }
+      )))
+    )
+  }, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -33,11 +47,9 @@ function Sidebar() {
       </div>
       <div className="sidebar__chats">
           <SidebarChat addNewChat />
-          <SidebarChat />
-          <SidebarChat />
-          <SidebarChat />
-          <SidebarChat />
-          <SidebarChat />
+          {rooms.map(room =>(
+            <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+          ))}
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD9xoq2Zj2VXrkAANXXAFDIE_AM3zB00dg",
   authDomain: "whatsapp-clone-86484.firebaseapp.com",
@@ -5,5 +7,17 @@ const firebaseConfig = {
   storageBucket: "whatsapp-clone-86484.appspot.com",
   messagingSenderId: "671349875115",
   appId: "1:671349875115:web:e5a324b83fcb8329f772ee",
-  measurementId: "G-KVYH9V7EE0"
+  measurementId: "G-KVYH9V7EE0",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
